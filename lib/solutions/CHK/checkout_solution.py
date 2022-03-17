1b9c36ffe2358f7cbfa66ceffc81af5d0dfbd183
sbx_user1051 <sbx_user1051@169.254.132.213>
2022-03-17 23:12:24 +0000

    Thu Mar 17 23:12:24 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,39 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    return 0
-    # raise NotImplementedError()
+    a_count = 0
+    b_count = 0
+    c_count = 0
+    d_count = 0
+    total_price = 0
+    if skus == "" or skus == "-":
+        return -1
+    for c in skus:
+        if c == 'A':
+            a_count +=1
+            if a_count % 3 == 0:
+                total_price += 30
+            else:
+                total_price += 50
+        elif c == 'B':
+            b_count +=1
+            if b_count % 2 == 0:
+                total_price += 15
+            else:
+                total_price += 30
+        elif c == 'C':
+            c_count +=1
+            total_price += 20
+        elif c == 'D':
+            d_count +=1
+            total_price += 15
+        else:
+            return -1
+
+    return total_price
+
+# assert(checkout("ABCDCBAABCABBAAA") == 505)
+# assert(checkout("") == -1)
+# assert(checkout("-") == -1)
+# assert(checkout("-1") == -1)
+# assert(checkout("AABAB") == 175)
